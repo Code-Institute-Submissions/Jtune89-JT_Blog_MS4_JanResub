8488c87f589bd9122529887a0fc5ed97b5f3d1ca
Jtune89 <Joseph.tune1@gmail.com>
2022-11-21 20:29:31 +0000

    settings update

--- politics/settings.py
+++ politics/settings.py
@@ -60,6 +60,8 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap 4'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
